4edc8196792326c3a370588bb08eaa8d8837c593
Uzair <uzairali19b@gmail.com>
2021-05-03 21:45:27 +0500

    Part 1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,26 @@
-def hello_world
-  puts 'Hello World!'
+# Part 1
+
+def bubble_sort(array)
+  index = 0
+
+  while index < array.size - 1 do
+    if array[index] > array[index+1]
+      array[index], array[index+1] = array[index+1], array[index]
+      index = 0
+    else
+      index += 1
+    end
+  end
+
+  print array
 end
+
+bubble_sort([4,3,78,2,0,2]) # => [0, 2, 2, 3, 4, 78]
+
+
+# Part 2
+
+# bubble_sort_by(["hi","hello","hey"]) do |left,right|
+# left.length - right.length
+# end
+# ["hi", "hey", "hello"]
