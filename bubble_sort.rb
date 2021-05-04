f8d3d5ff42eba2459cf8864e316233c88aaaaad5
Uzair <uzairali19b@gmail.com>
2021-05-04 12:49:06 +0500

    bubble sort by update

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,7 +19,19 @@ bubble_sort([4, 3, 78, 2, 0, 2]) # => [0, 2, 2, 3, 4, 78]
 
 # Part 2
 
-# bubble_sort_by(["hi","hello","hey"]) do |left,right|
-# left.length - right.length
-# end
-# ["hi", "hey", "hello"]
+def bubble_sort_by(array)
+  index = 0
+
+  while index < array.size - 1
+    if array[index].length > array [index + 1].length
+        array[index], array[index + 1] = array[index + 1], array[index]
+      index = 0
+    else
+      index += 1
+    end
+  end
+
+  print array
+end
+
+bubble_sort_by(["hey", "hello", "hi","asdf","anyother"])  # => ["hi", "hey", "hello"]
