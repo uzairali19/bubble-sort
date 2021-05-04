9b540dc826ee19463ec922adb9a6d74f932da9ee
Anasscodes <rockbell8@gmail.com>
2021-05-04 13:42:41 +0300

    Add bubble_sort_by test

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -20,18 +20,24 @@ bubble_sort([4, 3, 78, 2, 0, 2]) # => [0, 2, 2, 3, 4, 78]
 # Part 2
 
 def bubble_sort_by(array)
-  index = 0
-
-  while index < array.size - 1
-    if array[index].length > array[index + 1].length
-      array[index], array[index + 1] = array[index + 1], array[index]
-      index = 0
-    else
-      index += 1
+  swap = true
+  while swap do
+  i = 0
+  swap = false
+  while i < array.size - 1
+    if (yield array[i], array[i + 1]).to_i >= 0
+      array[i], array[i + 1] = array[i + 1], array[i]
+      swap = true
+    end
+      i += 1
+     
     end
   end
-
   print array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left, right|
+  left.length - right.length
 end
 
 bubble_sort_by(%w[hey hello hi]) # => ["hi", "hey", "hello"]
